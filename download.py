7dad107ae6b7784031206f465109b6183b7666c9
ragnarocker13 <112006063+ragnarocker13@users.noreply.github.com>
2023-02-02 16:30:45 +0800

    modified download.py

--- download.py
+++ download.py
@@ -6,6 +6,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 # In[35]:
@@ -23,6 +24,9 @@ driver = webdriver.Chrome(chrome_options=options)
 # Load URL and click download link by link text
 driver.get("https://jobs.homesteadstudio.co/data-engineer/assessment/download/")
 driver.find_element(By.LINK_TEXT, "Download").click()
+
+time.sleep(6)
+driver.close()
 
 
 # In[ ]:
